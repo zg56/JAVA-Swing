44b68e4a3d105577ea739322bcca388d4021b454
zg56 <zg56@drexel.edu>
2017-12-26 17:45:24 +0100

    Update Main.java

--- Snake-And-Ladders/Main.java
+++ Snake-And-Ladders/Main.java
@@ -1,5 +1,6 @@
 package snakes;
-
+// Zoltan Gercsak - Snakes and ladders game
+// This Main class is the driver for the game, calls the Board constructor to initialize it
 public class Main {
 
 	public static void main(String args[]) {
